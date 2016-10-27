a9e9069d7830aba9d1b631c2c7ab61eaee2b0a8a
Volney Casas <volneyrock@gmail.com>
2016-10-27 21:53:11 -0200

    criada tabela de amigos

--- models/tabelas.py
+++ models/tabelas.py
@@ -3,6 +3,17 @@ Post = db.define_table('post',
             Field("corpo", "text", label="Mensagem"), # Campo do texto
             auth.signature) # Campos para auditoria de registros
 
-# Validadores para Post
+## Validadores para Post
 Post.corpo.requires = [IS_NOT_EMPTY(error_message="Campo obrigatório"),
     IS_LENGTH(140)]
+
+## Lista da situação de amizade
+## P = Pendente, A = Aprovada, R = Rejeitada, B = Bloqueada
+SIT = ["P", "A", "R", "B"]
+Amigos = db.define_table('amigos',
+            Field('solicitante', 'reference auth_user'),
+            Fiel('solicitado', 'reference auth_user'),
+            Field('situacao', 'string' default = 'P'),
+            auth.signature)
+## Validadores para amigos
+Amigos.situacao.requires = IS_IN_SET(SIT)
